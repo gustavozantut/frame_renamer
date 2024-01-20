e7e3a9a420c79e9a533dcb8ddbae3d9051e393e8
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2024-01-20 19:52:06 -0300

    sadgasdgsad

--- app/frame_renamer.py
+++ app/frame_renamer.py
@@ -49,7 +49,7 @@ def main():
             for filename in sorted([os.path.join(frames_dir, file) for file in os.listdir(frames_dir) if os.path.isfile(os.path.join(frames_dir, file))]):
                 
                 copyfile(frames_dir / filename, stream_frames_dir / f"frame_{frame_count}.png")
-                move(frames_dir / "renamed_frames" / filename)
+                move(frames_dir / filename, frames_dir / "renamed_frames" / filename)
                 frame_count += 1
                 
             time.sleep(0.5)
